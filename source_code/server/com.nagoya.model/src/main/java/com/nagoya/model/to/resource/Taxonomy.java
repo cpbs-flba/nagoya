65e83806159a36267611c00e51421a16e8e7d5d0
Florin Balint <florin.balint@cpb-software.com>
2018-12-21 14:52:34 +0100

    adding id to taxonomy to

--- source_code/server/com.nagoya.model/src/main/java/com/nagoya/model/to/resource/Taxonomy.java
+++ source_code/server/com.nagoya.model/src/main/java/com/nagoya/model/to/resource/Taxonomy.java
@@ -11,8 +11,23 @@ public class Taxonomy implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	private String id;
 	private Taxonomy parent;
 	private String name;
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
 
 	/**
 	 * @return the parent
