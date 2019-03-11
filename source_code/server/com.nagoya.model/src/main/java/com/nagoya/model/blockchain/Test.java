6eb9561364995aec310fe22220e3a7b643a46bd6
Florin Balint <balint.florin@gmail.com>
2019-03-11 23:52:28 +0100

    registration & token confirmation

---
+++ source_code/server/com.nagoya.model/src/main/java/com/nagoya/model/blockchain/Test.java
@@ -0,0 +1,15 @@
+package com.nagoya.model.blockchain;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Test {
+
+	public static void main(String[] args) extends Application {
+		// TODO Auto-generated method stub
+
+	}
+
+}
